ac6c9ef1a24c71d59f0d0849665da96572329469
amoshydra <amoswwj@gmail.com>
2016-12-22 03:32:04 +0800

    Restore morgan to production

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var logger = require('morgan');
 
 var app = express();
 
@@ -8,12 +9,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-if (app.get('env') === 'development') {
-  var logger = require('morgan');
-  app.use(logger('dev'));
-}
-
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
