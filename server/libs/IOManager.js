33e5d5957db2259eec4ba5311cc0ca52757be35b
amoshydra <amoswwj@gmail.com>
2016-12-21 12:28:58 +0800

    Implement navigation gesture

--- server/libs/IOManager.js
+++ server/libs/IOManager.js
@@ -28,6 +28,24 @@ const IOManager = function IOManager(server) {
         robot.mouseClick('left', true);
       }
 
+      if (event === 'navigatemove') {
+        let angle = Math.atan2(pos.pageY, pos.pageX);
+        angle = angle * 180 / Math.PI + 45;
+        let quadrant = Math.floor(angle/90) % 4 + 1;
+
+        switch (quadrant) {
+          case 0: robot.keyTap("up"); break;
+          case 1: robot.keyTap("right"); break;
+          case 2: robot.keyTap("down"); break;
+          case 3: robot.keyTap("left"); break;
+          case -1: robot.keyTap("left"); break;
+        }
+      }
+
+      if (event === 'navigateenter') {
+        robot.keyTap("enter");
+      }
+
     });
   });
 
